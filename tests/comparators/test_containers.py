313dc77d3fd7028541d82f4af6a8d34d6ddd47cb
Maria Glukhova <siamezzze@gmail.com>
2017-02-13 15:07:39 +0200

    Change the tests for nested containers so they reflect the latest changes.
    Change and expand the tests for containers -- test different
    types and different levels of nested containers.

--- tests/comparators/test_containers.py
+++ tests/comparators/test_containers.py
@@ -19,19 +19,14 @@
 
 import pytest
 
-try:
-    import tlsh
-    tlsh_found = True
-except ImportError:
-    tlsh_found = False
-
 from diffoscope.comparators.utils.compare import compare_files
 
-from utils.data import data, load_fixture
+from utils.data import data, load_fixture, get_data
 from utils.tools import skip_unless_tools_exist
 
 test_tar_gz = load_fixture('test1.tar.gz')
-test_zip_bz2 = load_fixture('test1.tar.bz2')
+test_zip_bz2 = load_fixture('test2.zip.bz2')
+test_zip = load_fixture('test2.zip')
 test_tar = load_fixture('test3.tar')
 test_cpio = load_fixture('test3.cpio')
 
@@ -42,6 +37,10 @@ def differences_containers(test_tar, test_cpio):
 @pytest.fixture
 def differences_nested(test_tar_gz, test_zip_bz2):
     return compare_files(test_tar_gz, test_zip_bz2).details
+
+@pytest.fixture
+def differences_different_levels(test_tar_gz, test_zip):
+    return compare_files(test_tar_gz, test_zip).details
 
 @skip_unless_tools_exist('cpio')
 def test_file_list(differences_containers):
@@ -63,19 +62,60 @@ def test_content(differences_containers):
     assert differences_containers[2].unified_diff == expected_diff
 
 @skip_unless_tools_exist('bzip2', 'gzip', 'zipinfo')
-@pytest.mark.skipif(not tlsh_found, reason='tlsh is missing')
 def test_types_nested(differences_nested):
-    expected_diff = \
-        open(data('nested_containers_types_expected_diff')).read()
+    expected_diff = get_data('nested_containers_types_expected_diff')
     assert differences_nested[0].source1 == 'container type'
     assert differences_nested[0].source2 == 'container type'
     assert differences_nested[0].unified_diff == expected_diff
 
-@skip_unless_tools_exist('bzip2', 'gzip')
-@pytest.mark.skipif(not tlsh_found, reason='tlsh is missing')
+@skip_unless_tools_exist('bzip2', 'gzip', 'zipinfo')
+def test_types_nested1(differences_nested):
+    expected_diff1 = get_data('nested_containers_types_expected_diff1')
+    assert differences_nested[2].details[0].source1 == 'container type'
+    assert differences_nested[2].details[0].source2 == 'container type'
+    assert differences_nested[2].details[0].unified_diff == expected_diff1
+
+@skip_unless_tools_exist('bzip2', 'gzip', 'zipinfo')
+def test_file_list_nested(differences_nested):
+    expected_diff = get_data('nested_containers_filelist_expected_diff')
+    assert differences_nested[1].source1 == 'file list'
+    assert differences_nested[1].source2 == 'file list'
+    assert differences_nested[1].unified_diff == expected_diff
+
+@skip_unless_tools_exist('bzip2', 'gzip', 'zipinfo')
 def test_file_list_nested2(differences_nested):
-    expected_diff = \
-        open(data('nested_containers_filelist_expected_diff')).read()
-    assert differences_nested[1].details[0].source1 == 'file list'
-    assert differences_nested[1].details[0].source2 == 'file list'
-    assert differences_nested[1].details[0].unified_diff == expected_diff
+    expected_diff = get_data('nested_containers_filelist_expected_diff1')
+    assert differences_nested[2].details[1].source1 == 'file list'
+    assert differences_nested[2].details[1].source2 == 'file list'
+    assert differences_nested[2].details[1].unified_diff == expected_diff
+
+@skip_unless_tools_exist('bzip2', 'gzip', 'zipinfo')
+def test_content_diff_nested(differences_nested):
+    expected_diff = get_data('text_ascii_expected_diff')
+    assert differences_nested[2].details[2].source1 == 'dir/text'
+    assert differences_nested[2].details[2].source2 == 'dir/text'
+    assert differences_nested[2].details[2].unified_diff == expected_diff
+
+@skip_unless_tools_exist('gzip', 'zipinfo')
+def test_types_different_levels(differences_different_levels):
+    f = open(data('nested_containers_types_expected_diff2'), 'w')
+    f.write(differences_different_levels[0].unified_diff)
+    f.close()
+    expected_diff = get_data('nested_containers_types_expected_diff2')
+    assert differences_different_levels[0].source1 == 'container type'
+    assert differences_different_levels[0].source2 == 'container type'
+    assert differences_different_levels[0].unified_diff == expected_diff
+
+@skip_unless_tools_exist('gzip', 'zipinfo')
+def test_file_list_different_levels(differences_different_levels):
+    expected_diff = get_data('nested_containers_filelist_expected_diff2')
+    assert differences_different_levels[1].source1 == 'file list'
+    assert differences_different_levels[1].source2 == 'file list'
+    assert differences_different_levels[1].unified_diff == expected_diff
+
+@skip_unless_tools_exist('gzip', 'zipinfo')
+def test_content_diff_different_levels(differences_different_levels):
+    expected_diff = get_data('text_ascii_expected_diff')
+    assert differences_different_levels[2].source1 == 'dir/text'
+    assert differences_different_levels[2].source2 == 'dir/text'
+    assert differences_different_levels[2].unified_diff == expected_diff
